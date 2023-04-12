0e79a8e8456b0e3f1f4000904e5214b0b9ddd9d4
LuisBalan <jlbalcz@gmail.com>
2023-04-12 01:37:58 -0600

    add validators pipes to filter

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class GetTasksFilterDto {
+  @IsString()
+  @IsOptional()
   search?: string;
+  
+  @IsEnum(TaskStatus)
+  @IsOptional()
   status?: TaskStatus;
 }
